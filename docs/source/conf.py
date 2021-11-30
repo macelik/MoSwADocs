f0cd3a3b54ec824dd44150e06b68ea1c3fe5ac00
macelik <41537897+macelik@users.noreply.github.com>
2021-11-30 11:26:12 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MoSwA'
+copyright = '2021, Muhammet Celik'
+author = 'Muhammet Celik, Shan Tharanga, Kaushal Kumar, Mohammad Asif Khan'
 
 release = '0.1'
 version = '0.1.0'
