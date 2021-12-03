38071b1fac2040254bbf843ba4f87fc5d55d54e3
macelik <41537897+macelik@users.noreply.github.com>
2021-12-03 15:38:56 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
